61884d4ef8a30ee198732f23ea48dbd981f58da8
deepforce <zyq81678593@126.com>
2018-12-06 14:57:59 -0800

    Detele Files

--- Backend/ParkingAdvisor/home_page/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def show(request):
-
-    return render(request, "home_page.html")
--- Backend/ParkingAdvisor/launch_page/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LaunchPageConfig(AppConfig):
-    name = 'launch_page'
